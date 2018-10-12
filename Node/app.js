5d30325eb7901a4dbecec4513796e3fa4ba8e3b2
Plopo <michael.fabre@epsi.fr>
2018-10-12 08:49:40 +0200

    Send, Get, Change Object

--- Node/app.js
+++ Node/app.js
@@ -1,6 +1,26 @@
 const port = 3333;
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+
+// Send an Object
+app.route('/api/cats').post((req, res) => {
+  res.send(201, req.body);
+});
+
+// Change an Object
+app.route('/api/cats/:name').put((req, res) => {
+  res.send(200, req.body);
+});
+
+// Get an Object
+app.route('/api/cats/:name').get((req, res) => {
+  const requestedCatName = req.params['name'];
+  res.send({ name: requestedCatName });
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
